lucario6607/lc0fish

use attention operators from com.microsoft

src/neural/onnx/builder.h
@@ -124,6 +124,13 @@ class OnnxBuilder {
   std::string Mish(const std::string& name, const std::string& input);
   std::string Sqrt(const std::string& name, const std::string& input);
   std::string Reciprocal(const std::string& name, const std::string& input);
+  std::string Attention(const std::string& name, const std::string& input,
+                        const std::string& weights, const std::string& bias,
+                        const std::string& rel_pos_bias, int heads);
+  std::string MultiHeadAttention(const std::string& name, const std::string& Q,
+                                 const std::string& K, const std::string& V,
+                                 const std::string& bias,
+                                 const std::string& rel_pos_bias, int heads);
   // Returns ONNX model as protobuf.
   const pblczero::ModelProto& as_proto() const { return model_; }
   // Returns serialized model.
@@ -132,6 +139,7 @@ class OnnxBuilder {
  private:
   const int opset_;
   pblczero::ModelProto model_;
+  bool import_com_microsoft_ = true;
 };
 
 }  // namespace lczero

borg323/lc0

src/neural/onnx/builder.h
@@ -126,6 +126,13 @@ class OnnxBuilder {
                    pblczero::TensorProto::DataType type);
   std::string ReduceMean(const std::string& name, const std::string& input,
                          std::initializer_list<int> axes, bool keepdims = true);
+  std::string Attention(const std::string& name, const std::string& input,
+                        const std::string& weights, const std::string& bias,
+                        const std::string& attention_bias, int heads);
+  std::string MultiHeadAttention(const std::string& name, const std::string& Q,
+                                 const std::string& K, const std::string& V,
+                                 const std::string& bias,
+                                 const std::string& attention_bias, int heads);
   // Returns ONNX model as protobuf.
   const pblczero::ModelProto& as_proto() const { return model_; }
   // Returns serialized model.
@@ -134,6 +141,7 @@ class OnnxBuilder {
  private:
   const int opset_;
   pblczero::ModelProto model_;
+  bool import_com_microsoft_ = true;
 };
 
 }  // namespace lczero