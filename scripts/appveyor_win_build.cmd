ae4835f7bb1c6d5436577b91239525856c5ef454
borg323 <borg323@users.noreply.github.com>
2025-12-13 23:19:09 +0200

    test

--- scripts/appveyor_win_build.cmd
+++ scripts/appveyor_win_build.cmd
@@ -21,7 +21,9 @@ cd ..
 IF %PGO%==true msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=PGOptimize /p:DebugInformationFormat=ProgramDatabase /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 IF %NAME%==onnx (
   ren build\lc0.exe lc0-trt.exe
-  meson configure build -Ddefault_backend= -Dcudnn_libdirs= -Dgtest=%GTEST%
+  meson configure build -Ddefault_backend= -Dcudnn_libdirs=
+  IF %PGO%==true msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=PGOptimize /p:DebugInformationFormat=ProgramDatabase /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+  IF %PGO%==false msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=true /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   IF %PGO%==true msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=PGOptimize /p:DebugInformationFormat=ProgramDatabase /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   IF %PGO%==false msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=true /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   ren build\lc0.exe lc0-dml.exe
