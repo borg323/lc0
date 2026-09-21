d760908288c6f641a531a465654d6099dd9c998a
borg323 <borg323@users.noreply.github.com>
2026-09-21 07:40:30 +0300

    fix dxc_helper.py exit code

--- src/neural/backends/dx/shaders/dxc_helper.py
+++ src/neural/backends/dx/shaders/dxc_helper.py
@@ -10,5 +10,5 @@ for i in range(len(x)):
   if re.match(r"[a-zA-Z]:/", x[i]):
     x[i] = os.path.normpath(x[i])
 # We asssume dxc is already on the path.
-os.system('dxc ' + '"{}"'.format('" "'.join(x)))
-
+ret = os.system('dxc ' + '"{}"'.format('" "'.join(x)))
+sys.exit(ret >> 8 if os.name != 'nt' else ret)
